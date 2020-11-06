3d5164cc49198f25fb4ee31ce79d4dab8273002a
Eddy <eddy.hoevenaers@sii.be>
2020-11-06 16:41:18 +0100

    Jira EUCAPTCHA-28: removed hardcoded values

--- src/main/java/com/sii/eucaptcha/security/JwtToken.java
+++ src/main/java/com/sii/eucaptcha/security/JwtToken.java
@@ -3,6 +3,7 @@ package com.sii.eucaptcha.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 
@@ -15,12 +16,18 @@ import java.security.Key;
 public class JwtToken {
 
     /**
+     * Captcha JwtToken subject -> application.properties
+     */
+    @Value("${captcha.jwt.token.subject}")
+    private String subject;
+
+    /**
      * Generating Token
      * @param key
      * @return jwtToken
      */
     public String generateJwtToken(Key key) {
-        String jwtString = Jwts.builder().setSubject("euCaptcha").signWith(SignatureAlgorithm.HS512, key).compact();
+        String jwtString = Jwts.builder().setSubject(subject).signWith(SignatureAlgorithm.HS512, key).compact();
         return jwtString;
     }
 
@@ -30,7 +37,7 @@ public class JwtToken {
      * @return true|false
      */
     public Boolean verifyToken(String jwtToken , Key key) throws SignatureException {
-        if (Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken).getBody().getSubject().equals("euCaptcha")) {
+        if (Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken).getBody().getSubject().equals(subject)) {
             return true;
         } else {
             return false;
