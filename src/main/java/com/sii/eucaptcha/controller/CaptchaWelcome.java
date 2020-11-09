8df93030cfbdff5b686c84d54478cc6f5c548a3f
Eddy <eddy.hoevenaers@sii.be>
2020-11-09 07:41:51 +0100

    Jira EUCAPTCHA-34:removed Swagger API

--- src/main/java/com/sii/eucaptcha/controller/CaptchaWelcome.java
+++ src/main/java/com/sii/eucaptcha/controller/CaptchaWelcome.java
@@ -3,14 +3,13 @@ package com.sii.eucaptcha.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import springfox.documentation.annotations.ApiIgnore;
+
 /**
  * @author mousab.aidoud
  * @version 1.0
  * Get the captcha welcome page .
  */
 @Controller
-@ApiIgnore
 public class CaptchaWelcome {
 
     @Value("${captcha.welcome.page}")
