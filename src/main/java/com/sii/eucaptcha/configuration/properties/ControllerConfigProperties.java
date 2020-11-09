4af7dd05e6c3c227cf959199c0815dad02fbf415
Eddy <eddy.hoevenaers@sii.be>
2020-11-09 22:56:34 +0100

    Jira EUCAPTCHA-26: removed hardcoded values

--- src/main/java/com/sii/eucaptcha/configuration/properties/ControllerConfigProperties.java
+++ src/main/java/com/sii/eucaptcha/configuration/properties/ControllerConfigProperties.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath: controller.properties")
+@PropertySource("classpath:controller.properties")
 @ConfigurationProperties(prefix = "controller.captcha")
 public class ControllerConfigProperties {
 }
