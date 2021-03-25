215fbba188825551778795a72dfadb9690916719
Eddy <eddy.hoevenaers@sii.be>
2021-03-25 10:57:12 +0100

    Fixed Cors issue permanently, fixed language issue for spoken Captcha and created a new war file

--- src/main/java/com/sii/eucaptcha/configuration/WebConfig.java
+++ src/main/java/com/sii/eucaptcha/configuration/WebConfig.java
@@ -112,13 +112,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler(props.getJsHandler())
                 .addResourceLocations(props.getJsLocations());
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("OPTIONS", "GET", "POST")
-                .exposedHeaders("x-jwtString", "Content-Type")
-                .allowedHeaders("x-jwtString", "Content-Type", HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
-    }
 }
