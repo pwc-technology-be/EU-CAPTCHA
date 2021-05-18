fe09e96b641390dbd8711134a71d62a8bc79fcee
Eddy <eddy.hoevenaers@sii.be>
2021-05-18 16:49:11 +0200

    Github issue 83: fixed 403 errors and created new war file

--- src/main/java/com/sii/eucaptcha/controller/CaptchaController.java
+++ src/main/java/com/sii/eucaptcha/controller/CaptchaController.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 import com.sii.eucaptcha.controller.constants.CaptchaConstants;
 import com.sii.eucaptcha.controller.dto.captcharesult.CaptchaResultDto;
 import com.sii.eucaptcha.controller.dto.captchaquery.CaptchaQueryDto;
-import com.sii.eucaptcha.exceptions.ForbiddenException;
 import com.sii.eucaptcha.security.JwtToken;
 import com.sii.eucaptcha.service.CaptchaService;
 import io.swagger.annotations.ApiParam;
@@ -26,7 +25,6 @@ import java.util.Locale;
 public class CaptchaController {
 
     private final CaptchaService captchaService;
-    private final JwtToken jwtToken;
 
     /**
      * Captcha answer length -> controller.properties
@@ -39,9 +37,8 @@ public class CaptchaController {
     @Value("${controller.captcha.idLength}")
     private int captchaIdLength;
 
-    public CaptchaController(CaptchaService captchaService, JwtToken jwtToken) {
+    public CaptchaController(CaptchaService captchaService) {
         this.captchaService = captchaService;
-        this.jwtToken = jwtToken;
     }
 
 
@@ -53,9 +50,9 @@ public class CaptchaController {
      */
     @CrossOrigin
     @GetMapping(value = "/captchaImg")
-    public CaptchaResultDto getCaptchaImage(@ApiParam() Locale locale ,
-                                            @ApiParam()  Integer captchaLength ,
-                                            @ApiParam(defaultValue = CaptchaConstants.STANDARD) String captchaType ,
+    public CaptchaResultDto getCaptchaImage(@ApiParam() Locale locale,
+                                            @ApiParam() Integer captchaLength,
+                                            @ApiParam(defaultValue = CaptchaConstants.STANDARD) String captchaType,
                                             @ApiParam() Integer degree) {
 
 
@@ -79,10 +76,9 @@ public class CaptchaController {
     @GetMapping(value = "/reloadCaptchaImg/{previousCaptchaId}")
     public CaptchaResultDto reloadCaptchaImage(@PathVariable("previousCaptchaId") String previousCaptchaId,
                                                @ApiParam() Locale locale,
-                                               @ApiParam()  Integer captchaLength ,
-                                               @ApiParam(defaultValue = CaptchaConstants.STANDARD) String captchaType ,
-                                               @ApiParam() Integer degree,
-                                               @RequestHeader("x-jwtString") String jwtString) {
+                                               @ApiParam() Integer captchaLength,
+                                               @ApiParam(defaultValue = CaptchaConstants.STANDARD) String captchaType,
+                                               @ApiParam() Integer degree) {
 
         CaptchaQueryDto captchaQueryDto = new CaptchaQueryDto.CaptchaQueryDtoBuilder(captchaType)
                 .captchaLength(captchaLength)
@@ -90,17 +86,7 @@ public class CaptchaController {
                 .locale(locale)
                 .degree(degree)
                 .build();
-        try {
-            // Reload the captcha if the token is valid
-            if (jwtToken.verifyToken(jwtString)) {
-                return captchaService.generateCaptchaWrapper(captchaQueryDto);
-               // return captchaService.generateCaptchaImage(previousCaptchaId, locale);
-            } else {
-               throw new ForbiddenException();
-            }
-        } catch (Exception e) {
-            throw new ForbiddenException();
-        }
+        return captchaService.generateCaptchaWrapper(captchaQueryDto);
     }
 
     /**
@@ -112,33 +98,23 @@ public class CaptchaController {
      */
     @CrossOrigin
     @PostMapping(value = "/validateCaptcha/{captchaId}")
-    public ResponseEntity<String> validateCaptcha(@PathVariable("captchaId") String captchaId,
+    public ResponseEntity<String> validateCaptcha(@PathVariable(value = "captchaId", required = false) String captchaId,
                                                   @RequestParam(value = "captchaAnswer", required = false) String captchaAnswer,
                                                   @RequestParam(value = "useAudio", required = false) boolean useAudio,
-                                                  @RequestParam(value = "captchaType" , required = true , defaultValue = CaptchaConstants.STANDARD ) String captchaType,
-                                                  @RequestHeader("x-jwtString") String jwtString ) {
+                                                  @RequestParam(value = "captchaType", defaultValue = CaptchaConstants.STANDARD) String captchaType) {
 
 
         //Verify the validity of the captcha answer.
-        if (/*captchaAnswer.trim().length() != captchaAnswerLength || */
-                captchaId.trim().length() != captchaIdLength
-                || jwtString == null) {
+        if (captchaId.trim().length() != captchaIdLength) {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-
         } else {
             try {
-                if (jwtToken.verifyToken(jwtString)) {
-                    boolean responseCaptcha;
-                    //the Token is valid , we proceed the validation of the captcha
-                    responseCaptcha = captchaService.validateCaptcha(captchaId, captchaAnswer , captchaType ,  useAudio );
-                    JsonObject response = new JsonObject();
-                    //response captcha ( valid -> success || invalid -> fail  )
-                    response.addProperty("responseCaptcha", responseCaptcha ? "success" : "fail");
-                    return new ResponseEntity<>(response.toString(), HttpStatus.OK);
-                } else {
-                    //The token is not valid.
-                    return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-                }
+                boolean responseCaptcha;
+                responseCaptcha = captchaService.validateCaptcha(captchaId, captchaAnswer, captchaType, useAudio);
+                JsonObject response = new JsonObject();
+                //response captcha ( valid -> success || invalid -> fail  )
+                response.addProperty("responseCaptcha", responseCaptcha ? "success" : "fail");
+                return new ResponseEntity<>(response.toString(), HttpStatus.OK);
             } catch (Exception e) {
                 return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
