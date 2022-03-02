3aa839087c95220fdd6d28c1d99f8af81aeca8a5
Eddy <ehv@sii.be>
2022-03-02 12:38:08 +0100

    Revert "Updating Angular packages"
    This reverts commit 79dd7146361ee136ee24c42bc67447189408c77f.
    
    # Conflicts:
    #	clientfiles/Eu-Captcha-Angular/package.json

--- src/main/java/com/sii/eucaptcha/controller/CaptchaController.java
+++ src/main/java/com/sii/eucaptcha/controller/CaptchaController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 /**
@@ -49,6 +50,10 @@ public class CaptchaController {
                                             @RequestParam(defaultValue = "true", required = false) boolean capitalized,
                                             @RequestParam(required = false) Integer degree) {
 
+        if (StringUtils.isBlank(locale) || StringUtils.equalsIgnoreCase("Undefined", locale)) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Locale is missing or invalid!");
+        }
+
         CaptchaQueryDto captchaQueryDto = new CaptchaQueryDto.CaptchaQueryDtoBuilder(captchaType)
                 .captchaLength(captchaLength)
                 .locale(locale)
@@ -74,6 +79,15 @@ public class CaptchaController {
                                                @RequestParam(defaultValue = CaptchaConstants.STANDARD, required = false) String captchaType,
                                                @RequestParam(required = false) boolean capitalized,
                                                @RequestParam(required = false) Integer degree) {
+
+        if (StringUtils.isBlank(locale) || StringUtils.equalsIgnoreCase("Undefined", locale)) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Locale is missing or invalid!");
+        }
+
+        if (StringUtils.isBlank(previousCaptchaId)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CaptchaId is missing!");
+        }
+
 
         CaptchaQueryDto captchaQueryDto = new CaptchaQueryDto.CaptchaQueryDtoBuilder(captchaType)
                 .captchaLength(captchaLength)
